b032bbdbcca7b7ab2f517817535605b00b6546f3
Felix Kastner <bio2hazard@gmail.com>
2015-06-05 22:45:49 -0700

    Webjobs-Test

---
+++ FisHarmonyJob/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace FisHarmonyJob
+{
+  public class Functions
+  {
+    // This function will get triggered/executed when a new message is written 
+    // on an Azure Queue called queue.
+    public static void ProcessBlob([BlobTrigger("test/{name}")] TextReader input, TextWriter log)
+    {
+      log.WriteLine(input.ReadToEnd());
+    }
+  }
+}
