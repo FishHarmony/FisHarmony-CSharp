2fabadafbe0b2d485543ba8b1cda785e218a5e09
Felix Kastner <bio2hazard@gmail.com>
2015-06-06 01:21:43 -0700

    Image Loading

--- FisHarmonyJob/Functions.cs
+++ FisHarmonyJob/Functions.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.IO;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 using Microsoft.Azure.WebJobs;
 
 namespace FisHarmonyJob
@@ -26,6 +28,22 @@ namespace FisHarmonyJob
       var text = input.ReadToEnd();
       log.WriteLine(text);
       output = text;
+    }
+
+    public static void LoadImage([BlobTrigger("image/{name}")] Stream inputImage,
+      TextWriter log)
+    {
+      Image image = Image.FromStream(inputImage);
+      int count = 0;
+      foreach (var propertyItem in image.PropertyItems)
+      {
+        log.WriteLine("Property Item " + count.ToString());
+        log.WriteLine("ID: 0x" + propertyItem.Id.ToString("x"));
+        log.WriteLine("type: " + propertyItem.Type.ToString());
+        log.WriteLine("value: " + propertyItem.Value.ToString());
+        log.WriteLine("length:" + propertyItem.Len.ToString() + " bytes" );
+        count++;
+      }
     }
   }
 }
