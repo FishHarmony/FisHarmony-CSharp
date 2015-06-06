1e680d9dfce4ce97fd52e69f74df76806200ee66
Felix Kastner <bio2hazard@gmail.com>
2015-06-05 23:18:04 -0700

    Webjobs

--- FisHarmonyJob/Functions.cs
+++ FisHarmonyJob/Functions.cs
@@ -8,16 +8,24 @@ namespace FisHarmonyJob
   {
     // This function will get triggered/executed when a new message is written 
     // on an Azure Queue called queue.
-    public static async Task ProcessBlob([BlobTrigger("test/{name}.{ext}")] Stream input, string name, string ext, TextWriter log)
-    {
-      log.WriteLine("Blob name:" + name);
-      log.WriteLine("Blod extension:" + ext);
+    //public static async Task ProcessBlob([BlobTrigger("test/{name}.{ext}")] Stream input, string name, string ext, TextWriter log)
+    //{
+    //  log.WriteLine("Blob name:" + name);
+    //  log.WriteLine("Blod extension:" + ext);
 
-      using (StreamReader reader = new StreamReader(input))
-      {
-        string blobContent = await reader.ReadToEndAsync();
-        log.WriteLine("Blob content: {0}", blobContent);
-      }
+    //  using (StreamReader reader = new StreamReader(input))
+    //  {
+    //    string blobContent = await reader.ReadToEndAsync();
+    //    log.WriteLine("Blob content: {0}", blobContent);
+    //  }
+    //}
+    public static void CopyBlob([BlobTrigger("input/{name}")] TextReader input,
+    [Blob("output/{name}")] out string output,
+      TextWriter log)
+    {
+      var text = input.ReadToEnd();
+      log.WriteLine(text);
+      output = text;
     }
   }
 }
